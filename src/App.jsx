65b427a540a5d503d0c3d5f76fb1febab79fff9a
Francisco Timez <franciscotimez@gmail.com>
2022-06-19 18:45:40 -0300

    Inicio navbar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { NavBar } from './components/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <NavBar />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const NavBar = () => {
+  return (
+    <ul>
+        <li><a>Cosas</a></li>
+        <li><a>Cosas</a></li>
+        <li><a>Cosas</a></li>
+        <li><a>Cosas</a></li>
+    </ul>
+  )
+}
